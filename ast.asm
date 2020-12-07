411fa8d420cb35f1512e18e04828c53c5090c28d
Anemona Maria M <57135826+anemonamaria@users.noreply.github.com>
2020-12-07 13:22:25 +0200

    Add files via upload

--- ast.asm
+++ ast.asm
@@ -19,96 +19,84 @@ global create_tree
 global iocla_atoi
  
 iocla_atoi: 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
     enter   0, 0
-    mov     edi, [ebp + 8]
+    mov     esi, [ebp + 8]          ; realizam o copie a string-ului dat in input
     xor     ecx, ecx
     xor     eax, eax
+    xor     edx, edx
+    push    edi
     
-cat_timp:
-    movzx   esi, byte [edi]   ;zero extension
-    cmp     esi, dword 0      ; daca string-ul s-a terminat iesim din functie
-    je      gata
+while1:
+    movzx   edi, byte [esi]     
+    test    edi, edi                ; daca string-ul ramas este null inseamna ca
+    je      gata                    ; am terminat verificarea
+    add     esi, 1
+
+    cmp     edi, '-'                ; daca este '-' trebuie sa retinem pentru a
+    je      negativ                 ; inmulti la final cu -1
+    cmp     edi, '0'                ; caracterele cu codurile ascii < 0 nu ne
+    jl      return                  ; intereseaza
+    cmp     edi, '9'                ; caracterele cu codurile ascii > 9 nu ne
+    jg      return                  ; intereseaza
     
-    cmp     esi, 45 ;ascii for '-'
-    je      minus
-    cmp     esi, 48 ;ascii for '0'
-    jl      urmatorul_nr
-    cmp     esi, 57; ascii for '9'
-    jg      urmatorul_nr
+    sub     edi, '0'
+    mov     ebx, 10
+    imul    ebx                     ; inmultim numarul format cu 10 ca sa putem
+    add     eax, edi                ; adauga cifra la care ne aflam
+    jmp     while1
     
-    sub     esi, 48
-    mov     edx, 10
-    imul    edx
-    add     eax, esi
-    
-    inc     edi
-    jmp     cat_timp
-    
-minus:
-    mov     ecx, 1
-    inc     edi
-    jmp     cat_timp  
+negativ:
+    mov     ecx, 1                  ; nr negativ (ecx = 0 pt nr pozitive)
+    jmp     while1  
  
-urmatorul_nr:
-    xor     eax, eax
-    
 gata:
-    cmp     ecx, 0
-    je      return
-    mov     edx, -1
-    imul    edx
+    cmp     ecx, 0                  ; verificam daca avem un numar negativ 
+    je      return                  
+    imul    eax, -1
+    pop     edi
     
 return:
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
     leave
     ret
 
 create_tree:
-    ; TODO
     enter   0, 0 
     pusha
     xor     eax, eax
     mov     [root], dword 0
 
-    ; Nod 1
-    mov     eax, dword [ebp + 8]
-   ; PRINTF32 `eax %s\n\x0`, eax
+    mov     eax, dword [ebp + 8]   ; realizam o copie a string-ului dat in input
     push    eax
     call    strdup
     add     esp, 4
-    ;PRINTF32 `eax %s\n\x0`, eax
 
-    mov     ecx, eax ; impartim in primul token string-ul
+    mov     ecx, eax 
     push    delim
     push    ecx
-    call    strtok
+    call    strtok                 ; aflam primul token (root)
     add     esp, 8
 
     push    eax
     call    strdup
     add     esp, 4
     mov     ebx, eax
-
-
     push    12
     call    malloc
     add     esp, 4
   
-    mov     [eax], ebx      ; creeam nodul
+    mov     [eax], ebx              ; facem un nod cu tokenul aflat
     mov     [eax + 4], dword 0
     mov     [eax + 8], dword 0
     mov     [root], eax
-
-    push    eax
+    push    eax                     ; urcam pe stiva root-ul
 
     push    delim
     push    dword 0
     call    strtok
     add     esp, 8
-    ;PRINTF32 `root %s\n\x0`, ebx
-while:
-    push    eax
+
+while2:                             ; folosim un while pentru a afla
+    push    eax                     ; toate nodurile
     call    strdup
     add     esp, 4
     mov     ebx, eax
@@ -116,97 +104,72 @@ while:
     push    12
     call    malloc
     add     esp, 4
-    mov     [eax], ebx      ; creeam nodul
+    mov     [eax], ebx              ; nodul ce va fi introdus in structura
     mov     [eax + 4], dword 0
     mov     [eax + 8], dword 0
 
-    ;PRINTF32 `caracter din while  %s\n\x0`, ebx
-    cmp     byte  [ebx], '+'
-    je      urca_pe_stiva
+    cmp     byte [ebx], '+'         ; verificam daca cumva nodul contine
+    je      urca_pe_stiva           ; operatori si ii urcam pe stiva
     cmp     byte [ebx], '-'
     je      caz_special
     cmp     byte [ebx], '*'
     je      urca_pe_stiva
     cmp     byte [ebx], '/'
     je      urca_pe_stiva
-    jne     next       ; daca ajunge aici inseamna ca avem un 
+    jne     next                    ; avem numar, nu il urcam in stiva
 
 caz_special:
     push    eax
     push    ebx
     call    strlen
     add     esp, 4
-    cmp     eax, 1
-    pop     eax
+
+    cmp     eax, 1                  ; verificam daca '-' vine de la diferenta
+    pop     eax                     ; sau de la un numar negativ
     je      urca_pe_stiva
     jne     next
+
 continue: 
-    ;PRINTF32 `la ce ne aflam %s\n\x0`, ebx
     push    delim
     push    dword 0
     call    strtok
     add     esp, 8
     
-    ;PRINTF32 `\n\n\x0`
     cmp     eax, 0
-    jne     while
+    jne     while2
     jmp     exit
 
 urca_pe_stiva:
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
     pop     ecx
-    ;PRINTF32 `urca pe stiva   %s\n\x0`, [ecx]
     cmp     [ecx + 4], dword 0
     je      adauga_operator_stanga
-    mov     [ecx + 8], eax      ;daca stanga nu e libera inseamna ca cel din dreapta e liber
-    
-    jmp     salt
+    mov     [ecx + 8], eax          ; daca stanga este ocupata inseamna ca 
+    jmp     salt                    ; pozitia din dreapta este sigur libera
+
 adauga_operator_stanga:
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-    ;PRINTF32 `adauga op stanga\n\x0`
     mov     [ecx + 4], eax
+    push    ecx                    
 
-
-    push    ecx
 salt:
-
-    ;PRINTF32 `salt\n\x0`
     push    eax
     jmp     continue
 
 next:
-
-    pop     ecx
-    ;PRINTF32 `next   %s\n\x0`, [ecx]
-    cmp     [ecx + 4], dword 0
+    pop     ecx                     ; scoatem operatorul din stiva daca
+    cmp     [ecx + 4], dword 0      ; are ambii fii ocupati
     je      adauga_frunza_stanga
-    ;PRINTF32 `frunza dreapta\n\x0`
+    mov     [ecx + 8], eax          ; daca stanga este ocupata inseamna ca 
+    jmp     salt2                   ; pozitia din dreapta este sigur libera
 
-    mov     [ecx + 8], eax      ;daca stanga nu e libera inseamna ca cel din dreapta e liber
-    
-    jmp     salt2
 adauga_frunza_stanga:
-    ;PRINTF32 `frunza stanga\n\x0`
     mov     [ecx + 4], eax
     push    ecx
+
 salt2:
-    ;PRINTF32 `salt2\n\x0`
     jmp     continue
 
 exit:
-    ;PRINTF32 `exit\n\x0`
-   ; mov     eax, [root]
-    ;push    0
-   ; push    eax
-   ; call    printTree
-    ;add     esp, 8
-    ;push    eax
-    ;call    print_tree_preorder
-
-    ;add     esp, 4
-    
-   ; pop     eax
     popa
     leave
-    mov     eax, [root]
+    mov     eax, [root]             ; returnam structura in eax
     ret
